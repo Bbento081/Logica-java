e2639fdcfe139679ecc840abcc6a8dd3167e4f17
Bbento081 <bbento081@gmail.com>
2025-08-22 18:42:25 -0300

    commit

---
+++ Exercícios/Poo/Aluno.java
@@ -0,0 +1,12 @@
+package Poo;
+
+public class Aluno {
+
+    private String nome;
+    private long matricula;
+    private String discp1;
+    private String discp2;
+    private String discp3;
+
+
+}
